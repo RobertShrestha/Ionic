91a955a5d80edf75316a88a17c0cc9e1efea749e
Robert Shrestha <robertshrestha2068@gmail.com>
2021-10-01 11:24:54 +0545

    Initial

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import '@codetrix-studio/capacitor-google-auth';
+import { Plugins } from '@capacitor/core';
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+ userInfo = null;
+ 
+  constructor() { }
+ 
+  async googleSignup() {
+    const googleUser = await Plugins.GoogleAuth.signIn(null) as any;
+    console.log('my user: ', googleUser);
+    this.userInfo = googleUser;
+  }
+
+}
